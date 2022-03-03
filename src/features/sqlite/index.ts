3eea873c11cd2132f1db4cbcc6fc3d92fa620858
Marco Bonomo <marco.bonomo@app-quality.com>
2022-03-03 11:11:56 +0100

    add concat support

--- src/features/sqlite/index.ts
+++ src/features/sqlite/index.ts
@@ -1,71 +1,42 @@
 const sqlite3 = require("better-sqlite3");
-const util = require("util");
 
 const db = new sqlite3(":memory:");
+db.function("NOW", () => "datetime('now')");
+db.function("CONCAT", { varargs: true }, (...args: string[]) => args.join(""));
+
 const mockDb: any = {};
 
-mockDb.createTable = (table: string, columns: string[]) => {
-  return new Promise(async (resolve, reject) => {
-    const query = `CREATE TABLE IF NOT EXISTS ${table} (${columns.join(
-      ", "
-    )});`;
-
-    try {
-      await db.exec(query);
+const originalRun = db.run.bind(db);
+db.run = (query: string) => {
+  return new Promise((resolve, reject) => {
+    return originalRun(query, function (err: any) {
+      if (err) {
+        reject(err.message);
+      }
+      //@ts-ignore
+      if (this && this.lastID) {
+        //@ts-ignore
+        resolve(this.lastID);
+      }
       resolve(true);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-mockDb.dropTable = (table: string) => {
-  return new Promise(async (resolve, reject) => {
-    const query = `DROP TABLE IF EXISTS ${table};`;
-    try {
-      await db.exec(query);
-      resolve(true);
-    } catch (error) {
-      reject(error);
-    }
+    });
   });
 };
 
-mockDb.all = (query: string): Promise<any> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const data = await db.prepare(query).all();
-      resolve(data);
-    } catch (err) {
-      console.log(query);
-      console.log("asd");
-      reject(err);
-    }
-  });
+db.createTable = (table: string, columns: string[]) => {
+  const query = `CREATE TABLE IF NOT EXISTS ${table} (${columns.join(", ")});`;
+  return db.run(query);
 };
-mockDb.get = (query: string): Promise<any> => {
-  return new Promise(async (resolve, reject) => {
-    const data = await db.prepare(query).get();
-    resolve(data);
-  });
+db.dropTable = (table: string) => {
+  const query = `DROP TABLE IF EXISTS ${table};`;
+  return db.run(query);
+};
+db.insert = (table: string, data: { [key: string]: any }) => {
+  const query = `
+    INSERT INTO ${table} (${Object.keys(data).join(
+    ", "
+  )}) VALUES ("${Object.values(data).join('", "')}");`;
+  return db.run(query);
 };
 
-mockDb.run = (query: string): Promise<any> => {
-  return new Promise(async (resolve, reject) => {
-    const data = await db.prepare(query).run();
-    resolve(data);
-  });
-};
-
-mockDb.insert = (table: string, data: any): Promise<any> => {
-  return new Promise(async (resolve, reject) => {
-    const sql = `INSERT INTO ${table} (${Object.keys(data)
-      .map((d) => d)
-      .join(",")}) VALUES (${Object.keys(data)
-      .map(() => "?")
-      .join(",")});`;
-    const res = await db.prepare(sql).run(...Object.values(data));
-    resolve(res);
-  });
-};
-
-export default mockDb;
+export default db;
