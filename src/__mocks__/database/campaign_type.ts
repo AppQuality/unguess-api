82b07005d4fc99f99f581405bc0db09725f43e52
cicababba <cicababba1@gmail.com>
2022-04-07 12:36:56 +0200

    refactor(get-workspace-campaigns): refactored tests based on new standards

---
+++ src/__mocks__/database/campaign_type.ts
@@ -0,0 +1,32 @@
+import sqlite3 from "@src/features/sqlite";
+
+const db = sqlite3("tryber");
+
+export const table = {
+  create: async () =>
+    await db.createTable("wp_appq_campaign_type", [
+      "id int(11)",
+      "name varchar(45)",
+      "type int(11)",
+    ]),
+  drop: async () => {
+    await db.dropTable("wp_appq_campaign_type");
+  },
+};
+
+const data: {
+  [key: string]: (params?: any) => Promise<{ [key: string]: any }>;
+} = {};
+
+data.basicItem = async (params) => {
+  const item = {
+    id: 1,
+    name: "Banana campaign",
+    type: 1,
+    ...params,
+  };
+  await db.insert("wp_appq_campaign_type", item);
+  return item;
+};
+
+export { data };
