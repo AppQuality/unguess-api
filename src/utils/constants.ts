5dc1ee2409d4dff3898ebd8283815010232b67e8
Luca Cannarozzo <cannarozzoluca@live.it>
2022-06-27 11:30:34 +0200

    feat(post-campaign): add coin availability check before cp creation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,6 +10,8 @@ export const DT_TV = 5;
 export const FUNCTIONAL_CAMPAIGN_TYPE_ID = 0;
 export const EXPERIENTIAL_CAMPAIGN_TYPE_ID = 1;
 
+export const DEFAULT_EXPRESS_COST = 1;
+
 export interface ResponseError {
   message: string;
   code: number;
