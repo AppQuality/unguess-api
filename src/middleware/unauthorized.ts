e3a65c25f23a15cf46118a4505e42fc11adb783e
Marco Bonomo <marco.bonomo@unguess.io>
2022-09-23 13:08:48 +0200

    fix(debug): enable auth debug only if env debug is 1

--- src/middleware/unauthorized.ts
+++ src/middleware/unauthorized.ts
@@ -1,9 +1,9 @@
-import { Context } from 'openapi-backend';
-import process from 'process';
+import { Context } from "openapi-backend";
+import process from "process";
 
-export default async (c : Context, req : Request, res : OpenapiResponse) => {
+export default async (c: Context, req: Request, res: OpenapiResponse) => {
   res.skip_post_response_handler = true;
-  if (process.env && process.env.DEBUG) {
+  if (process.env && process.env.DEBUG && process.env.DEBUG === "1") {
     console.log(c.security);
   }
   return res.status(403).json({
