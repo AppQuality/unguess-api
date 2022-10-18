3ee65da1e9eae280df7ab63d74a445f68882578b
Luca Cannarozzo <cannarozzoluca@live.it>
2022-10-18 15:52:38 +0200

    fix(cp-outputs): fix view query

--- src/__mocks__/database/cp_outputs_view.ts
+++ src/__mocks__/database/cp_outputs_view.ts
@@ -7,19 +7,18 @@ interface CampaignParams {
 }
 
 const viewQuery = `
-SELECT b.campaign_id, b.bugs, m.media
+SELECT c.id as campaign_id, b.bugs, m.media
 FROM wp_appq_evd_campaign c
-    LEFT JOIN (SELECT c.id as campaign_id, COUNT(b.id) as bugs
-            FROM wp_appq_evd_bug b
-            JOIN wp_appq_evd_campaign c ON (c.id = b.campaign_id)
-            GROUP BY c.id) b on b.campaign_id = c.id
-    LEFT JOIN (SELECT c.id as campaign_id, COUNT(t.id) as media
-                     FROM wp_appq_evd_campaign c
-                              JOIN wp_appq_campaign_task t ON (t.campaign_id = c.id)
-                              JOIN wp_appq_user_task_media m ON (m.campaign_task_id = t.id)
-                     GROUP BY c.id
-                ) m on (m.campaign_id = c.id)
-GROUP BY b.campaign_id`;
+         LEFT JOIN (SELECT c1.id as campaign_id, COUNT(b.id) as bugs
+                    FROM wp_appq_evd_bug b
+                             JOIN wp_appq_evd_campaign c1 ON (c1.id = b.campaign_id)
+                    GROUP BY c1.id) b on (b.campaign_id = c.id)
+         LEFT JOIN (SELECT c2.id as campaign_id, COUNT(t.id) as media
+                    FROM wp_appq_evd_campaign c2
+                             JOIN wp_appq_campaign_task t ON (t.campaign_id = c2.id)
+                             JOIN wp_appq_user_task_media m ON (m.campaign_task_id = t.id)
+                    GROUP BY c2.id) m on (m.campaign_id = c.id)
+GROUP BY c.id`;
 
 class CampaignOutputs extends View<CampaignParams> {
   protected name = "campaigns_outputs";
