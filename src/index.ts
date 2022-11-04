0213c76e7abebb05252d61f6e593c8c3670e1c4f
Davide Bizzi <davide.bizzi@app-quality.com>
2022-11-04 10:38:59 +0100

    feat: Log config on debug mode

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import app from "./app";
 import config from "./config";
 
 const PORT = config.port || 3000;
+
+console.log(`DEBUG : '${process.env.DEBUG}'`);
+if (process.env.DEBUG === "1") console.log(config);
+
 app.listen(PORT, () => console.info("api listening on port " + PORT));
