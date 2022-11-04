3ded2e0cc4d5c92db85ccd7c0c94ee5786e575b0
Davide Bizzi <davide.bizzi@app-quality.com>
2022-11-04 10:19:52 +0100

    ci: Fix env variables

--- deployment/after-install.sh
+++ deployment/after-install.sh
@@ -44,7 +44,7 @@ services:
       PORT: 80
       API_ROOT: '$API_ROOT'
       DB_HOST: '$DB_HOST'
-      DB_PORT: '$DB_HOST'
+      DB_PORT: '$DB_PORT'
       DB_USER: '$DB_USER'
       DB_PASSWORD: '$DB_PASSWORD'
       DB_NAME: '$DB_NAME'
