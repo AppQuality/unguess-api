83de66ae279455d4655db1a9e239eea4575455b4
Marco Bonomo <marco.bonomo@unguess.io>
2022-11-08 15:12:48 +0100

    fix(log): add verbose log behavior only if env DEBUG = 1

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import config from "./config";
 const PORT = config.port || 3000;
 
 console.log(`DEBUG : '${process.env.DEBUG}'`);
-if (process.env.DEBUG === "1") console.log(config);
+if (process.env && process.env.DEBUG && process.env.DEBUG === "1")
+  console.log(config);
 
 app.listen(PORT, () => console.info("api listening on port " + PORT));
