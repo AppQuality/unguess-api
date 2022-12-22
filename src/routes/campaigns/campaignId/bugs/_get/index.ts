a7ea851d6aecebaca11e7b9b994f5c1bbd427cf9
Luca Cannarozzo <cannarozzoluca@live.it>
2022-12-22 14:45:40 +0100

    refactor(bugs): move route to class structure

--- src/routes/campaigns/campaignId/bugs/_get/index.ts
+++ src/routes/campaigns/campaignId/bugs/_get/index.ts
@@ -1,5 +1,4 @@
-/** OPENAPI-ROUTE: get-campaigns-cid-bugs */
-import { Context } from "openapi-backend";
+/** OPENAPI-CLASS: get-campaigns-cid-bugs */
 import {
   DEFAULT_ORDER_BY_PARAMETER,
   DEFAULT_ORDER_PARAMETER,
@@ -13,95 +12,132 @@ import {
   BugsOrderByValues,
   BugsOrderValues,
 } from "@src/utils/campaigns/getCampaignBugs";
-import { paginateItems } from "@src/utils/paginations";
+import UserRoute from "@src/features/routes/UserRoute";
 
-export default async (
-  c: Context,
-  req: OpenapiRequest,
-  res: OpenapiResponse
-) => {
-  let user = req.user;
-  let error = {
-    code: 500,
-    message: ERROR_MESSAGE,
-    error: true,
-  } as StoplightComponents["schemas"]["Error"];
+export default class BugsRoute extends UserRoute<{
+  response: StoplightOperations["get-campaigns-cid-bugs"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-cid-bugs"]["parameters"]["path"];
+  query: StoplightOperations["get-campaigns-cid-bugs"]["parameters"]["query"];
+}> {
+  private cp_id: number;
+  private prj_id?: number;
+  private showNeedReview: boolean = false;
+  private limit: number = LIMIT_QUERY_PARAM_DEFAULT;
+  private start: number = START_QUERY_PARAM_DEFAULT;
+  private order: string = DEFAULT_ORDER_PARAMETER;
+  private orderBy: string = DEFAULT_ORDER_BY_PARAMETER;
 
-  let cid = parseInt(c.request.params.cid as string);
+  constructor(configuration: RouteClassConfiguration) {
+    super(configuration);
 
-  const limit = c.request.query.limit
-    ? parseInt(c.request.query.limit as string)
-    : (LIMIT_QUERY_PARAM_DEFAULT as StoplightComponents["parameters"]["limit"]);
+    const params = this.getParameters();
+    this.cp_id = parseInt(params.cid as string);
 
-  const start = c.request.query.start
-    ? parseInt(c.request.query.start as string)
-    : (START_QUERY_PARAM_DEFAULT as StoplightComponents["parameters"]["start"]);
+    const query = this.getQuery();
+    if (query.limit) this.limit = parseInt(query.limit as unknown as string);
+    if (query.start) this.start = parseInt(query.start as unknown as string);
 
-  const order =
-    c.request.query.order &&
-    BugsOrderValues.includes((c.request.query.order as string).toUpperCase())
-      ? (c.request.query.order as string).toUpperCase()
-      : DEFAULT_ORDER_PARAMETER;
+    if (query.order && BugsOrderValues.includes(query.order))
+      this.order = query.order;
+    if (query.orderBy && BugsOrderByValues.includes(query.orderBy))
+      this.orderBy = query.orderBy;
+  }
 
-  const orderBy =
-    c.request.query.orderBy &&
-    BugsOrderByValues.includes(
-      (c.request.query.orderBy as string).toLowerCase()
-    )
-      ? (c.request.query.orderBy as string).toLowerCase()
-      : DEFAULT_ORDER_BY_PARAMETER;
-
-  res.status_code = 200;
-
-  try {
-    // Check if the campaign exists
-    let campaign = await getCampaign({ campaignId: cid });
+  protected async init(): Promise<void> {
+    const campaign = await getCampaign({ campaignId: this.cp_id });
 
     if (!campaign) {
-      error.code = 400;
-      throw error;
+      this.setError(400, {
+        code: 400,
+        message: "Campaign not found",
+      } as OpenapiError);
+
+      throw new Error("Campaign not found");
     }
 
-    // Check if user has permission to edit the campaign
-    await getProjectById({
-      projectId: campaign.project.id,
-      user: user,
-    });
-
-    // Get the campaign bugs
-    const bugs = await getCampaignBugs({
-      campaignId: cid,
-      user: user,
-      showNeedReview:
-        user.role === "administrator" ? true : campaign.showNeedReview,
-      limit,
-      start,
-      ...(order && { order }),
-      ...(orderBy && { orderBy }),
-      ...(req.query.filterBy !== undefined && {
-        filterBy: req.query.filterBy as { [key: string]: string | string[] },
-      }),
-    });
-
-    if (bugs && bugs.length) {
-      return await paginateItems({
-        items: bugs,
-        limit,
-        start,
-        total: bugs.length,
-      });
-    }
-
-    return await paginateItems({ items: [], total: 0 });
-  } catch (e: any) {
-    if (e.code) {
-      error.code = e.code;
-      res.status_code = e.code;
-    } else {
-      error.code = 500;
-      res.status_code = 500;
-    }
-
-    return error;
+    this.prj_id = campaign.project.id;
+    this.showNeedReview = campaign.showNeedReview;
   }
-};
+
+  private getProjectId(): number {
+    if (!this.prj_id) {
+      this.setError(400, {
+        code: 400,
+        message: "Project not defined",
+      } as OpenapiError);
+      throw new Error("Project not defined");
+    }
+
+    return this.prj_id;
+  }
+
+  private shouldShowNeedReview(): boolean {
+    if (this.getUser().role === "administrator") return true;
+
+    return this.showNeedReview;
+  }
+
+  protected async filter(): Promise<boolean> {
+    if (!(await super.filter())) return false;
+    // Check if user has permission to edit the campaign
+    try {
+      await getProjectById({
+        projectId: this.getProjectId(),
+        user: this.getUser(),
+      });
+    } catch (error) {
+      this.setError(403, {
+        code: 400,
+        message: "Project not found",
+      } as OpenapiError);
+      return false;
+    }
+
+    return true;
+  }
+
+  protected async prepare(): Promise<void> {
+    const req = this.configuration.request;
+
+    try {
+      // Check if the campaign exists
+
+      // Get the campaign bugs
+      const bugs = await getCampaignBugs({
+        campaignId: this.cp_id,
+        user: this.getUser(),
+        showNeedReview: this.shouldShowNeedReview(),
+        limit: this.limit,
+        start: this.start,
+        order: this.order,
+        orderBy: this.orderBy,
+        ...(req.query.filterBy !== undefined && {
+          filterBy: req.query.filterBy as { [key: string]: string | string[] },
+        }),
+      });
+
+      if (bugs && bugs.length) {
+        return this.setSuccess(200, {
+          items: bugs,
+          start: this.start,
+          limit: this.limit,
+          size: bugs.length,
+          total: bugs.length,
+        });
+      }
+
+      return this.setSuccess(200, {
+        items: [],
+        start: this.start,
+        limit: this.limit,
+        size: 0,
+        total: 0,
+      });
+    } catch (e: any) {
+      return this.setError(e.code || 500, {
+        message: e.message || ERROR_MESSAGE,
+        status_code: e.code || 500,
+      } as OpenapiError);
+    }
+  }
+}
