d92272d58176376e3674620719c9c68b61c03db3
it@app-quality.com <it@app-quality.com>
2022-12-22 15:42:36 +0100

    Modified src/reference/openapi.yml

---
+++ src/routes/campaigns/campaignId/bugs/bugId/_patch/index.spec.ts
@@ -0,0 +1,175 @@
+import app from "@src/app";
+import request from "supertest";
+import { adapter as dbAdapter } from "@src/__mocks__/database/companyAdapter";
+import { FUNCTIONAL_CAMPAIGN_TYPE_ID } from "@src/utils/constants";
+import bugType from "@src/__mocks__/database/bug_type";
+import bugs from "@src/__mocks__/database/bugs";
+
+const campaign_type_1 = {
+  id: 1,
+  name: "Functional Testing (Bug Hunting)",
+  type: FUNCTIONAL_CAMPAIGN_TYPE_ID,
+};
+const customer_1 = {
+  id: 1,
+  company: "Company 1",
+  company_logo: "logo999.png",
+  tokens: 100,
+};
+
+const user_to_customer_1 = {
+  wp_user_id: 1,
+  customer_id: 1,
+};
+
+const project_1 = {
+  id: 1,
+  display_name: "Project 999",
+  customer_id: 1,
+};
+const customer_2 = {
+  id: 2,
+  company: "Company 2",
+  company_logo: "logo999.png",
+  tokens: 100,
+};
+
+const user_to_customer_2 = {
+  wp_user_id: 2,
+  customer_id: 2,
+};
+
+const project_2 = {
+  id: 2,
+  display_name: "Project 2",
+  customer_id: 2,
+};
+
+const campaign_1 = {
+  id: 1,
+  start_date: "2017-07-20 10:00:00",
+  end_date: "2017-07-20 10:00:00",
+  close_date: "2017-07-20 10:00:00",
+  title: "Campaign 1 title",
+  customer_title: "Campaign 1 customer title",
+  status_id: 1,
+  is_public: 1,
+  campaign_type_id: campaign_type_1.id,
+  campaign_type: -1,
+  project_id: 1,
+};
+const campaign_2 = {
+  id: 2,
+  start_date: "2017-07-20 10:00:00",
+  end_date: "2017-07-20 10:00:00",
+  close_date: "2017-07-20 10:00:00",
+  title: "Campaign 2 title",
+  customer_title: "Campaign 2 customer title",
+  status_id: 1,
+  is_public: 1,
+  campaign_type_id: campaign_type_1.id,
+  campaign_type: -1,
+  project_id: 2,
+};
+const bug_1 = {
+  id: 1,
+  campaign_id: 1,
+  bug_type_id: 1,
+  title: "Bug 1 title",
+  description: "Bug 1 description",
+  status_id: 1,
+  priority_id: 1,
+  severity_id: 1
+}
+
+describe("GET /campaigns/{cid}/bug/{bid}", () => {
+  beforeAll(async () => {
+    await dbAdapter.add({
+      campaignTypes: [campaign_type_1],
+      campaigns: [campaign_1, campaign_2],
+      companies: [customer_1, customer_2],
+      projects: [project_1, project_2],
+      userToCustomers: [user_to_customer_1, user_to_customer_2],
+    });
+    await bugType.addDefaultItems();
+    await bugs.insert(bug_1);
+  });
+
+  // It should answer 403 if user is not logged in
+  it("Should answer 403 if user is not logged in", async () => {
+    const response = await request(app).patch(
+      `/campaigns/${campaign_1.id}/bug/${bug_1.id}`
+    );
+
+    expect(response.status).toBe(403);
+  });
+
+  // It should fail if the campaign does not exist
+  it("Should fail if the campaign does not exist", async () => {
+    const response = await request(app)
+      .patch(`/campaigns/999/bug/666`)
+      .set("Authorization", "Bearer user");
+
+    expect(response.status).toBe(400);
+  });
+
+  // it should return 200 if the user is the owner
+  it("Should return 200 if the user is the owner", async () => {
+    const response = await request(app)
+      .patch(`/campaigns/${campaign_1.id}/bug/${bug_1.id}`)
+      .set("Authorization", "Bearer user");
+    expect(response.status).toBe(200);
+  });
+
+  // it Should fail if the user is not the owner
+  it("Should fail if the user is not the owner", async () => {
+    const response = await request(app)
+      .patch(`/campaigns/${campaign_1.id}/bug/${bug_1.id}`)
+      .set("Authorization", "Bearer user");
+    expect(response.status).toBe(403);
+  });
+
+  //should return campaign bugTypes
+  it("Should return campaign bugTypes", async () => {
+    const response = await request(app)
+      .patch(`/campaigns/${campaign_1.id}/bug/${bug_1.id}`)
+      .set("Authorization", "Bearer user");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([
+      {
+        id: 1,
+        name: "Crash",
+      },
+      {
+        id: 3,
+        name: "Graphic",
+      },
+      {
+        id: 5,
+        name: "Performance",
+      },
+      {
+        id: 6,
+        name: "Malfunction",
+      },
+      {
+        id: 7,
+        name: "Typo",
+      },
+      {
+        id: 8,
+        name: "Other",
+      },
+      {
+        id: 9,
+        name: "Security",
+      },
+      {
+        id: 10,
+        name: "Usability",
+      },
+    ]);
+  });
+
+  // --- End of file
+});
