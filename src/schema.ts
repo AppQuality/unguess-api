6a113430e643e1bc8901256852517b226cdc620c
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-01-01 23:13:23 +0100

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -230,10 +230,6 @@ export interface components {
       };
       duplicated_of_id?: number;
       is_favorite?: number;
-      tags?: {
-        tag_id: number;
-        tag_name: string;
-      }[];
     };
     /**
      * BugAdditionalField
