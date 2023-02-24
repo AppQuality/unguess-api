ec675509c05b362d38ed2ee42fd8fb0d08831391
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2023-02-24 15:33:37 +0100

    get-campaigns-reports refactor complete

--- src/routes/campaigns/campaignId/reports/_get/index.ts
+++ src/routes/campaigns/campaignId/reports/_get/index.ts
@@ -1,16 +1,13 @@
 /** OPENAPI-CLASS: get-campaigns-reports */
 import CampaignRoute from "@src/features/routes/CampaignRoute";
-import { getCampaign, getCampaignReports } from "@src/utils/campaigns";
+import { getCampaignReports } from "@src/utils/campaigns";
 
 export default class Route extends CampaignRoute<{
-  response: StoplightOperations["get-campaigns-cid-meta"]["responses"]["200"]["content"]["application/json"];
-  parameters: StoplightOperations["get-campaigns-cid-meta"]["parameters"]["path"];
+  response: StoplightOperations["get-campaigns-reports"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-reports"]["parameters"]["path"];
 }> {
   protected async prepare() {
-    if (!(await getCampaign({ campaignId: this.cp_id })))
-      return this.setError(403, {} as OpenapiError);
-
-    const reports: any = await getCampaignReports(this.cp_id);
+    const reports = await getCampaignReports(this.cp_id);
     if (!reports) return this.setError(403, {} as OpenapiError);
 
     return this.setSuccess(200, reports);
