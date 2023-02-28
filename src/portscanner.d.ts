1e327f6c07b17e86ae8fe5ef5cc4050854504f28
Davide Bizzi <davide.bizzi@app-quality.com>
2023-02-28 12:32:05 +0100

    chore: Remove knex-specific files

--- src/portscanner.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "portscanner" {
-  export function checkPortStatus(
-    port: number,
-    host: string,
-    callback: (error: any, status: "open" | "closed") => void
-  ): void;
-}
