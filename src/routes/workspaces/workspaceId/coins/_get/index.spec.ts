fa92388c443a01d4455befc9c72fc2eabb3b8adf
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-06-01 15:38:36 +0200

    rework(get-workspace-coins): use knex

--- src/routes/workspaces/workspaceId/coins/_get/index.spec.ts
+++ src/routes/workspaces/workspaceId/coins/_get/index.spec.ts
@@ -1,85 +1,10 @@
 import app from "@src/app";
 import request from "supertest";
-import { adapter as dbAdapter } from "@src/__mocks__/database/companyAdapter";
 import { ERROR_MESSAGE } from "@src/utils/constants";
-
-const customer_1 = {
-  id: 1,
-  company: "Company",
-  company_logo: "logo.png",
-  tokens: 100,
-};
-
-const customer_2 = {
-  id: 2,
-  company: "Company",
-  company_logo: "logo.png",
-  tokens: 100,
-};
-
-const customer_3 = {
-  id: 43,
-  company: "Company",
-  company_logo: "logo.png",
-  tokens: 100,
-};
-
-const user_to_customer_1 = {
-  wp_user_id: 1,
-  customer_id: 1,
-};
-
-const user_to_customer_2 = {
-  wp_user_id: 1,
-  customer_id: 2,
-};
-
-const customer_profile_1 = {
-  id: 1,
-  wp_user_id: 1,
-  name: "Customer",
-  surname: "Customer",
-  email: "customer@unguess.io",
-};
-
-const coins_1 = {
-  id: 1,
-  customer_id: 1,
-  amount: 100,
-  price: 0,
-  created_on: "2022-06-24 12:47:30",
-  updated_on: "2022-06-24 12:51:23",
-};
-
-const coins_2 = {
-  ...coins_1,
-  id: 2,
-  amount: 50,
-};
-
-const coins_3 = {
-  ...coins_1,
-  id: 3,
-  customer_id: 2,
-};
+import useCustomersData from "./useCustomersData";
 
 describe("GET /workspaces/{wid}/coins", () => {
-  beforeAll(async () => {
-    return new Promise(async (res, rej) => {
-      try {
-        await dbAdapter.add({
-          profiles: [customer_profile_1],
-          companies: [customer_1, customer_2, customer_3],
-          userToCustomers: [user_to_customer_1, user_to_customer_2],
-          coins: [coins_1, coins_2, coins_3],
-        });
-      } catch (e) {
-        console.error(e);
-        rej(e);
-      }
-      res(true);
-    });
-  });
+  useCustomersData();
 
   it("Should return 403 status if user is not logged in", async () => {
     const response = await request(app).get("/workspaces/1/coins");
@@ -90,6 +15,7 @@ describe("GET /workspaces/{wid}/coins", () => {
     const response = await request(app)
       .get("/workspaces/1/coins")
       .set("authorization", "Bearer user");
+    console.log(response.body);
     expect(response.status).toBe(200);
   });
 
@@ -139,8 +65,22 @@ describe("GET /workspaces/{wid}/coins", () => {
 
     expect(response.body.items).toEqual(
       expect.arrayContaining([
-        expect.objectContaining(coins_1),
-        expect.objectContaining(coins_2),
+        expect.objectContaining({
+          id: 1,
+          customer_id: 1,
+          amount: 100,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
+        expect.objectContaining({
+          id: 2,
+          customer_id: 1,
+          amount: 50,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
       ])
     );
   });
@@ -153,7 +93,16 @@ describe("GET /workspaces/{wid}/coins", () => {
     expect(response.body.size).toBe(1);
 
     expect(response.body.items).toEqual(
-      expect.arrayContaining([expect.objectContaining(coins_2)])
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: 2,
+          customer_id: 1,
+          amount: 50,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
+      ])
     );
   });
 
@@ -165,7 +114,16 @@ describe("GET /workspaces/{wid}/coins", () => {
     expect(response.body.size).toBe(1);
 
     expect(response.body.items).toEqual(
-      expect.arrayContaining([expect.objectContaining(coins_1)])
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: 1,
+          customer_id: 1,
+          amount: 100,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
+      ])
     );
   });
 
@@ -176,8 +134,22 @@ describe("GET /workspaces/{wid}/coins", () => {
     expect(response.status).toBe(200);
     expect(response.body.items).toEqual(
       expect.arrayContaining([
-        expect.objectContaining(coins_1),
-        expect.objectContaining(coins_2),
+        expect.objectContaining({
+          id: 1,
+          customer_id: 1,
+          amount: 100,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
+        expect.objectContaining({
+          id: 2,
+          customer_id: 1,
+          amount: 50,
+          price: 0,
+          created_on: "2022-06-24 12:47:30",
+          updated_on: "2022-06-24 12:51:23",
+        }),
       ])
     );
   });
--- src/routes/workspaces/workspaceId/coins/_get/index.ts
+++ src/routes/workspaces/workspaceId/coins/_get/index.ts
@@ -1,13 +1,14 @@
 /** OPENAPI-ROUTE: get-workspaces-coins */
 import { Context } from "openapi-backend";
-import * as db from "@src/features/db";
-import { getWorkspace, getWorkspaceCoins } from "@src/utils/workspaces";
+import { fallBackCsmProfile } from "@src/utils/constants";
 import {
   ERROR_MESSAGE,
   LIMIT_QUERY_PARAM_DEFAULT,
   START_QUERY_PARAM_DEFAULT,
 } from "@src/utils/constants";
-import { paginateItems, formatCount } from "@src/utils/paginations";
+import { getGravatar } from "@src/utils/users/getGravatar";
+import { getTotalCoins } from "@src/utils/workspaces";
+import { tryber, unguess } from "@src/features/database";
 
 const fields = ["id", "customer_id", "amount", "price", "agreement_id"];
 const orders = ["ASC", "DESC"];
@@ -83,11 +84,14 @@ export default async (
       order,
     });
 
-    const countQuery = `SELECT COUNT(*) as count FROM wp_ug_coins c WHERE c.customer_id = ${wid}`;
-    let total = await db.query(countQuery, "unguess");
-    total = formatCount(total);
+    const countQuery = await unguess.tables.WpUgCoins.do()
+      .count("* as count")
+      .where("customer_id", wid)
+      .first();
 
-    return await paginateItems({
+    const total = countQuery.count ? countQuery.count : 0;
+
+    return paginateItems({
       items: prepareResponse(coins),
       start,
       limit,
@@ -95,6 +99,7 @@ export default async (
     });
   } catch (e: any) {
     if (e.code) {
+      console.log(e.message);
       error.code = e.code;
       res.status_code = e.code;
     } else {
@@ -105,3 +110,196 @@ export default async (
     return error;
   }
 };
+type PaginationParams = {
+  items: Array<any>;
+  limit?: StoplightComponents["parameters"]["limit"] | string;
+  start?: StoplightComponents["parameters"]["start"] | string;
+  total: number;
+};
+
+function paginateItems(data: PaginationParams) {
+  let { items, limit, start, total } = data;
+
+  if (typeof total === "string") {
+    total = parseInt(total);
+  }
+
+  return items.length
+    ? {
+        items,
+        start,
+        limit,
+        size: items.length,
+        total,
+      }
+    : emptyPaginatedResponse();
+}
+
+function emptyPaginatedResponse() {
+  return {
+    items: [],
+    start: 0,
+    limit: 0,
+    size: 0,
+    total: 0,
+  };
+}
+
+async function loadCsmData(
+  csm: StoplightComponents["schemas"]["Workspace"]["csm"]
+) {
+  let profilePic = await getGravatar(csm.email);
+  if (profilePic) csm.picture = profilePic;
+
+  return csm;
+}
+
+interface GetWorkspacesArgs {
+  user: UserType;
+  workspaceId?: number;
+}
+
+async function getWorkspace({
+  workspaceId,
+  user,
+}: GetWorkspacesArgs): Promise<
+  StoplightComponents["schemas"]["Workspace"] | false
+> {
+  let error = {
+    message: ERROR_MESSAGE + " with workspace",
+    error: true,
+  } as StoplightComponents["schemas"]["Error"];
+
+  // Check parameters
+  if (workspaceId == null || Number.isNaN(workspaceId) || workspaceId < 1)
+    throw { ...error, code: 400 };
+
+  if (user.role !== "administrator")
+    if (user.id == null || user.id <= 0) throw { ...error, code: 400 };
+
+  // Check if workspace exists
+
+  const customerSql = tryber.tables.WpAppqCustomer.do()
+    .select(
+      "wp_appq_customer.*",
+      tryber.ref("name").withSchema("wp_appq_evd_profile").as("csmName"),
+      tryber.ref("surname").withSchema("wp_appq_evd_profile").as("csmSurname"),
+      tryber.ref("email").withSchema("wp_appq_evd_profile").as("csmEmail"),
+      tryber.ref("id").withSchema("wp_appq_evd_profile").as("csmProfileId"),
+      tryber
+        .ref("wp_user_id")
+        .withSchema("wp_appq_evd_profile")
+        .as("csmTryberWpUserId"),
+      tryber.ref("user_url").withSchema("wp_users").as("csmUserUrl")
+    )
+    .leftJoin(
+      "wp_appq_evd_profile",
+      "wp_appq_evd_profile.id",
+      "wp_appq_customer.pm_id"
+    )
+    .leftJoin("wp_users", "wp_users.ID", "wp_appq_evd_profile.wp_user_id")
+    .where("wp_appq_customer.id", workspaceId)
+    .first();
+
+  let workspace = await customerSql;
+
+  if (workspace) {
+    if (user.role !== "administrator") {
+      // Check if user has permission to get the customer
+
+      const userToCustomerSql = tryber.tables.WpAppqUserToCustomer.do()
+        .select()
+        .where("wp_user_id", user.id || 0)
+        .where("customer_id", workspaceId)
+        .first();
+
+      let userToCustomer = await userToCustomerSql;
+
+      if (!userToCustomer) {
+        throw { ...error, message: "workspace issue", code: 403 };
+      }
+    }
+
+    //Add CSM info
+
+    let rawCsm = workspace.pm_id
+      ? {
+          id: workspace.pm_id,
+          name: workspace.csmName + " " + workspace.csmSurname,
+          email: workspace.csmEmail,
+          profile_id: workspace.csmProfileId,
+          tryber_wp_user_id: workspace.csmTryberWpUserId,
+          ...(workspace.csmUserUrl && { url: workspace.csmUserUrl }),
+        }
+      : fallBackCsmProfile;
+
+    let csm = await loadCsmData(rawCsm);
+
+    // Get workspace's express coins
+    const coins = await getWorkspaceCoins({ workspaceId });
+
+    // Get total coins amount
+    const totalCoins = await getTotalCoins(coins);
+
+    return {
+      id: workspace.id,
+      company: workspace.company,
+      tokens: workspace.tokens,
+      ...(workspace.company_logo && { logo: workspace.company_logo }),
+      csm: csm,
+      coins: totalCoins,
+    } as StoplightComponents["schemas"]["Workspace"];
+  }
+
+  return false;
+}
+
+interface GetWorkspacesCoinsArgs {
+  workspaceId: number;
+  limit?: number;
+  start?: number;
+  orderBy?: string;
+  order?: string;
+}
+
+/**
+ * Retrieve coins from a workspace
+ *
+ * Notes: we don't check if the user has permission to get the workspace, because this is done in the getWorkspace function
+ *
+ * @param workspaceId
+ * @returns array of coins
+ */
+async function getWorkspaceCoins({
+  workspaceId,
+  limit,
+  start,
+  orderBy,
+  order,
+}: GetWorkspacesCoinsArgs): Promise<StoplightComponents["schemas"]["Coin"][]> {
+  let error = {
+    message: ERROR_MESSAGE,
+    error: true,
+  } as StoplightComponents["schemas"]["Error"];
+
+  // Check parameters
+  if (workspaceId == null || workspaceId <= 0) throw { ...error, code: 400 };
+
+  // Retrieve coins packages
+
+  let query = unguess.tables.WpUgCoins.do()
+    .select()
+    .where("customer_id", workspaceId)
+    .where("amount", ">", 0);
+
+  if (order && orderBy) {
+    query = query.orderBy(orderBy, order);
+  }
+
+  if (limit || start) {
+    query = query.limit(limit || 10).offset(start || 0);
+  }
+  let packages = await query;
+
+  return packages.length ? packages : [];
+}
