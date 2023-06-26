6162dc3244bd3263c52ba5bbf78aa77c51067f39
Davide Bizzi <davide.bizzi@unguess.io>
2023-06-26 11:05:16 +0200

    feat: Allow sending multiple invites to workspaces

--- src/routes/workspaces/workspaceId/users/_post/index.ts
+++ src/routes/workspaces/workspaceId/users/_post/index.ts
@@ -1,183 +1,133 @@
 /** OPENAPI-CLASS: post-workspaces-wid-users */
+
+import { tryber } from "@src/features/database";
+import { sendTemplate } from "@src/features/mail/sendTemplate";
 import WorkspaceRoute from "@src/features/routes/WorkspaceRoute";
 import createTryberWPUser from "@src/features/wp/createTryberWPUser";
 import createUserProfile from "@src/features/wp/createUserProfile";
 import { randomString } from "@src/utils/users/getRandomString";
 import crypto from "crypto";
-import { sendTemplate } from "@src/features/mail/sendTemplate";
-import { tryber } from "@src/features/database";
-
-interface DbUser {
-  tryber_wp_id: number;
-  profile_id: number;
-  name: string;
-  surname: string;
-  email: string;
-  invitation_status: string;
-}
 
 export default class Route extends WorkspaceRoute<{
   response: StoplightOperations["post-workspaces-wid-users"]["responses"]["200"]["content"]["application/json"];
   parameters: StoplightOperations["post-workspaces-wid-users"]["parameters"]["path"];
   body: StoplightOperations["post-workspaces-wid-users"]["requestBody"]["content"]["application/json"];
 }> {
-  protected newUser: DbUser | undefined;
-  protected isPending: boolean = false;
-
   protected async filter(): Promise<boolean> {
-    if (!(await super.filter())) return false;
+    if (!(await super.filter())) {
+      return false;
+    }
 
     if (!this.isValidUser()) {
       this.setError(400, {} as OpenapiError);
       return false;
     }
+
     return true;
   }
 
   protected async prepare() {
-    const users = await this.getUsers();
-    const userInWorkspace = users.find((u) => u.email === this.getBody().email);
+    const userInCampaign = await this.retrieveUserAlreadyInvited();
 
-    if (userInWorkspace) {
-      this.isPending = !!(
-        userInWorkspace.invitation_status &&
-        Number.parseInt(userInWorkspace.invitation_status) !== 1
-      );
-
-      if (this.isPending) {
-        await this.sendInvitation({
-          email: userInWorkspace.email,
-          profile_id: userInWorkspace.profile_id,
-        });
-
-        return this.setSuccess(200, {
-          profile_id: userInWorkspace.profile_id,
-          tryber_wp_user_id: userInWorkspace.tryber_wp_id,
-          email: userInWorkspace.email,
-        });
-      } else {
-        // user already exists in the workspace
-        return this.setError(400, {
-          message: "User already exists",
-        } as OpenapiError);
-      }
+    if (userInCampaign) {
+      return this.prepareUserAlreadyInvited(userInCampaign);
     }
 
-    const userExists = await this.getUserByEmail(this.getBody().email);
-    let userToAdd: DbUser = userExists;
-
     try {
-      if (!userExists) {
-        userToAdd = await this.createUser(this.getBody());
-        this.newUser = userToAdd;
-
+      const userToAdd = await this.getUserToAdd();
+      if (userToAdd.newUser) {
         await this.sendInvitation({
           email: userToAdd.email,
           profile_id: userToAdd.profile_id,
+          type: "new_user",
         });
       } else {
-        this.notifyUser(userExists.email);
+        const userHasPendingInvitation =
+          userToAdd.invitation_status &&
+          Number(userToAdd.invitation_status) === 0;
+        this.notifyUser({
+          email: userToAdd.email,
+          type: userHasPendingInvitation ? "new_user" : "existing_user",
+        });
       }
-
       await this.addUserToWorkspace(userToAdd);
+
       return this.setSuccess(200, {
         profile_id: userToAdd.profile_id,
         tryber_wp_user_id: userToAdd.tryber_wp_id,
         email: userToAdd.email,
       });
     } catch (e) {
-      if (!userExists) this.removeCreatedUser(this.getBody().email);
-
       return this.setError(500, {
-        message: "Error adding user to workspace",
+        message: "Error adding user to campaign",
       } as OpenapiError);
     }
   }
 
-  private async addUserToWorkspace(user: DbUser): Promise<void> {
-    const { tryber_wp_id } = user;
-
-    await tryber.tables.WpAppqUserToCustomer.do().insert({
-      wp_user_id: tryber_wp_id,
-      customer_id: this.getWorkspaceId(),
-    });
-  }
-
-  private async removeCreatedUser(email: string): Promise<void> {
-    const user = await this.getUserByEmail(this.getBody().email);
-
-    if (user) {
-      await tryber.tables.WpAppqUserToCustomer.do().delete().where({
-        wp_user_id: user.tryber_wp_id,
-      });
+  private async prepareUserAlreadyInvited(userInCampaign: {
+    profile_id: number;
+    tryber_wp_id: number;
+    email: string;
+    invitation_status: string;
+  }) {
+    try {
+      await this.resendEmail(userInCampaign);
+    } catch (e) {
+      return this.setError(400, {
+        message: "User already exists",
+      } as OpenapiError);
     }
 
-    // Remove user profiles (if any)
-    await tryber.tables.WpUsers.do().delete().where({
-      user_email: email,
-    });
-
-    await tryber.tables.WpAppqEvdProfile.do().delete().where({
-      email: email,
+    return this.setSuccess(200, {
+      profile_id: userInCampaign.profile_id,
+      tryber_wp_user_id: userInCampaign.tryber_wp_id,
+      email: userInCampaign.email,
     });
   }
 
-  private async getUserByEmail(email: string): Promise<any> {
-    const alreadyRegisteredEmail = await tryber.tables.WpUsers.do()
-      .select(
-        tryber.ref("ID").withSchema("wp_users").as("tryber_wp_id"),
-        tryber.ref("id").withSchema("wp_appq_evd_profile").as("profile_id"),
-        tryber.ref("name").withSchema("wp_appq_evd_profile"),
-        tryber.ref("surname").withSchema("wp_appq_evd_profile"),
-        tryber.ref("email").withSchema("wp_appq_evd_profile")
+  private async getUserToAdd() {
+    const userExists = await this.getUserByEmail();
+    if (userExists) {
+      return { ...userExists, newUser: false };
+    }
+
+    try {
+      const userToAdd = await this.createUser(this.getBody());
+      return { ...userToAdd, newUser: true };
+    } catch (e) {
+      this.removeCreatedUser();
+      throw e;
+    }
+  }
+
+  private async resendEmail(userInCampaign: {
+    profile_id: number;
+    tryber_wp_id: number;
+    email: string;
+    invitation_status: string;
+  }) {
+    if (
+      !(
+        userInCampaign.invitation_status &&
+        Number.parseInt(userInCampaign.invitation_status) !== 1
       )
-      .join(
-        "wp_appq_evd_profile",
-        "wp_users.ID",
-        "wp_appq_evd_profile.wp_user_id"
+    ) {
+      throw new Error("User already exists");
+    }
+
+    await this.sendInvitation({
+      email: userInCampaign.email,
+      profile_id: userInCampaign.profile_id,
+      type: !!(
+        userInCampaign.invitation_status &&
+        Number.parseInt(userInCampaign.invitation_status) !== 1
       )
-      .where({
-        user_email: email,
-      });
-
-    return alreadyRegisteredEmail.length
-      ? {
-          ...alreadyRegisteredEmail[0],
-          invitation_status: "1",
-        }
-      : null;
-  }
-
-  private async createUser(
-    invitedUser: StoplightOperations["post-workspaces-wid-users"]["requestBody"]["content"]["application/json"]
-  ): Promise<DbUser> {
-    const { email, name = "", surname = "" } = invitedUser;
-    const psw = randomString(12);
-    const username =
-      name && surname ? `${name}-${surname}` : email.split("@")[0];
-
-    const tryber_wp_id = await createTryberWPUser(username, email, psw);
-
-    const profile = await createUserProfile({
-      tryber_wp_id,
-      name,
-      surname,
-      email,
+        ? "new_user"
+        : "existing_user",
     });
-
-    if (!profile) throw new Error("Error creating user profile");
-
-    return {
-      tryber_wp_id,
-      profile_id: profile.profile_id,
-      name,
-      surname,
-      email,
-      invitation_status: "0",
-    };
   }
 
-  protected async getUsers(): Promise<DbUser[]> {
+  private async retrieveUserAlreadyInvited() {
     const users = await tryber.tables.WpAppqUserToCustomer.do()
       .select(
         tryber.ref("id").withSchema("wp_appq_evd_profile").as("profile_id"),
@@ -204,19 +154,113 @@ export default class Route extends WorkspaceRoute<{
         "wp_appq_customer_account_invitations.tester_id"
       )
       .where("wp_appq_user_to_customer.customer_id", this.getWorkspaceId())
-      .groupBy("wp_appq_evd_profile.id");
-
-    if (!users) return [];
+      .where("wp_appq_evd_profile.email", this.getBody().email)
+      .groupBy("wp_appq_evd_profile.id")
+      .first();
 
     return users;
+  }
+
+  private async addUserToWorkspace({
+    tryber_wp_id,
+  }: {
+    tryber_wp_id: number;
+  }): Promise<void> {
+    await tryber.tables.WpAppqUserToCustomer.do().insert({
+      wp_user_id: tryber_wp_id,
+      customer_id: this.getWorkspaceId(),
+    });
+  }
+
+  private async removeCreatedUser() {
+    const user = await this.getUserByEmail();
+
+    if (user) {
+      await tryber.tables.WpAppqUserToCustomer.do().delete().where({
+        wp_user_id: user.tryber_wp_id,
+      });
+    }
+
+    // Remove user profiles (if any)
+    await tryber.tables.WpUsers.do().delete().where({
+      user_email: this.getBody().email,
+    });
+
+    await tryber.tables.WpAppqEvdProfile.do().delete().where({
+      email: this.getBody().email,
+    });
+  }
+
+  private async getUserByEmail() {
+    const alreadyRegisteredEmail = await tryber.tables.WpUsers.do()
+      .select(
+        tryber.ref("ID").withSchema("wp_users").as("tryber_wp_id"),
+        tryber.ref("id").withSchema("wp_appq_evd_profile").as("profile_id"),
+        tryber.ref("name").withSchema("wp_appq_evd_profile"),
+        tryber.ref("surname").withSchema("wp_appq_evd_profile"),
+        tryber.ref("email").withSchema("wp_appq_evd_profile"),
+        tryber
+          .ref("status")
+          .withSchema("wp_appq_customer_account_invitations")
+          .as("invitation_status")
+      )
+      .join(
+        "wp_appq_evd_profile",
+        "wp_users.ID",
+        "wp_appq_evd_profile.wp_user_id"
+      )
+      .leftJoin(
+        "wp_appq_customer_account_invitations",
+        "wp_appq_evd_profile.id",
+        "wp_appq_customer_account_invitations.tester_id"
+      )
+      .where({
+        user_email: this.getBody().email,
+      })
+      .first();
+
+    return alreadyRegisteredEmail;
+  }
+
+  private async createUser(invitedUser: {
+    email: string;
+    name?: string;
+    surname?: string;
+  }) {
+    const { email, name = "", surname = "" } = invitedUser;
+    const psw = randomString(12);
+    const username =
+      name && surname ? `${name}-${surname}` : email.split("@")[0];
+
+    const tryber_wp_id = await createTryberWPUser(username, email, psw);
+
+    const profile = await createUserProfile({
+      tryber_wp_id,
+      name,
+      surname,
+      email,
+    });
+
+    if (!profile) throw new Error("Error creating user profile");
+
+    return {
+      tryber_wp_id,
+      profile_id: profile.profile_id,
+      name,
+      surname,
+      email,
+      invitation_status: "0",
+    };
   }
 
   protected async sendInvitation({
     email,
     profile_id,
+    type = "new_user",
   }: {
     email: string;
     profile_id: number;
+    type?: "new_user" | "existing_user";
   }): Promise<void> {
     const token = crypto
       .createHash("sha256")
@@ -232,8 +276,12 @@ export default class Route extends WorkspaceRoute<{
       .onConflict("tester_id")
       .merge();
 
-    await this.notifyUser(email, {
-      "{Inviter.url}": `${this.getBaseURL()}/invites/${profile_id}/${token}`,
+    await this.notifyUser({
+      email,
+      type,
+      params: {
+        "{Inviter.url}": `${this.getBaseURL()}/invites/${profile_id}/${token}`,
+      },
     });
   }
 
@@ -246,13 +294,21 @@ export default class Route extends WorkspaceRoute<{
     return `${process.env.APP_URL}${locale}`;
   }
 
-  private async notifyUser(email: string, params?: { [key: string]: string }) {
+  private async notifyUser({
+    email,
+    type = "new_user",
+    params = {},
+  }: {
+    email: string;
+    type?: "new_user" | "existing_user";
+    params?: { [key: string]: string };
+  }) {
     const sender = this.getUser();
 
     await sendTemplate({
       email: email,
-      template: this.getEmailEvent(),
-      subject: this.getEmailSubject(),
+      template: this.getEmailEvent(type),
+      subject: this.getEmailSubject(type),
       optionalFields: {
         "{Inviter.name}": sender.email,
         "{Inviter.email}": sender.email,
@@ -264,10 +320,10 @@ export default class Route extends WorkspaceRoute<{
     });
   }
 
-  private getEmailSubject() {
+  private getEmailSubject(type: "new_user" | "existing_user") {
     const locale = this.getBody()?.locale ?? "en";
 
-    if (this.newUser || this.isPending) {
+    if (type === "new_user") {
       return locale === "it"
         ? `Entra in Unguess`
         : `You've been invited to join UNGUESS`;
@@ -278,10 +334,10 @@ export default class Route extends WorkspaceRoute<{
       : `You've been invited to join ${this.workspace?.company ?? "workspace"}`;
   }
 
-  private getEmailEvent() {
+  private getEmailEvent(type: "new_user" | "existing_user") {
     const body = this.getBody();
     const defaultEvent =
-      this.newUser || this.isPending
+      type === "new_user"
         ? "customer_invitation"
         : "customer_existent_invitation";
 
