95888fcadf9c40c78111009ee2f2cb9d2740eb6e
Marco Bonomo <marco.bonomo@unguess.io>
2023-06-30 15:09:05 +0200

    🔧 fix(mail.spec.ts): fix indentation and add missing semicolons ✨ feat(mail.spec.ts): add test case to check if mail contains the sender name

--- src/routes/workspaces/workspaceId/users/_post/mail.spec.ts
+++ src/routes/workspaces/workspaceId/users/_post/mail.spec.ts
@@ -85,6 +85,23 @@ describe("POST /workspaces/wid/users", () => {
       template_id: 4,
       last_editor_tester_id: 1,
     });
+
+    await tryber.tables.WpAppqUnlayerMailTemplate.do().insert({
+      id: 5,
+      html_body: "Test mail it from {Inviter.name}",
+      name: "Test mail",
+      json_body: "",
+      last_editor_tester_id: 1,
+      lang: "it",
+      category_id: 1,
+    });
+
+    await tryber.tables.WpAppqEventTransactionalMail.do().insert({
+      id: 5,
+      event_name: "customer_invitation_it_with_sender_name",
+      template_id: 5,
+      last_editor_tester_id: 1,
+    });
   });
 
   // Clear templates
@@ -235,6 +252,24 @@ describe("POST /workspaces/wid/users", () => {
         },
         subject: `Entra in ${workspaces.customer_1.company}`,
         categories: ["UNGUESSAPP_STAGING"],
+      })
+    );
+  });
+
+  it("Should send mail containing the sender name", async () => {
+    await request(app)
+      .post(`/workspaces/${workspaces.customer_1.id}/users`)
+      .set("Authorization", "Bearer user")
+      .send({
+        email: "paolo.gialli@example.com",
+        locale: "it",
+        event_name: "customer_invitation_it_with_sender_name",
+      });
+
+    expect(mockedSendgrid.send).toHaveBeenCalledTimes(1);
+    expect(mockedSendgrid.send).toHaveBeenCalledWith(
+      expect.objectContaining({
+        html: expect.stringContaining("Mario Rossi"),
       })
     );
   });
