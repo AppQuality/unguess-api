f49e6d17911b3e131846284ec36d77df9933ed9f
Marco Bonomo <marco.bonomo@unguess.io>
2023-11-03 12:29:06 +0100

    🐛 fix(getBugCustomStatus): fix SQL query syntax error and add missing fields to the result object 🐛 fix(getWidgetBugsByDuplicates): fix SQL query syntax error and add missing fields to the result object

--- src/utils/campaigns/getWidgetBugsByDuplicates/index.ts
+++ src/utils/campaigns/getWidgetBugsByDuplicates/index.ts
@@ -104,13 +104,32 @@ const getPriority = async (bug_id: number) => {
 const getCustomStatus = async (bug_id: number) => {
   const result = await db.query(
     db.format(
-      "SELECT cs.id, cs.name FROM wp_ug_bug_custom_status cs JOIN wp_ug_bug_custom_status_to_bug csb ON (csb.custom_status_id = cs.id) WHERE csb.bug_id=?",
+      `SELECT
+          cs.id,
+          cs.name,
+          cs.color,
+          cs.is_default,
+          csp.id as phase_id,
+          csp.name as phase_name,
+      FROM wp_ug_bug_custom_status cs 
+      JOIN wp_ug_bug_custom_status_phase csp ON (cs.phase_id = csp.id)
+      JOIN wp_ug_bug_custom_status_to_bug csb ON (csb.custom_status_id = cs.id) 
+      WHERE csb.bug_id=?`,
       [bug_id]
     ),
     "unguess"
   );
   if (!result.length) return DEFAULT_BUG_CUSTOM_STATUS;
-  return result[0] as { id: number; name: string };
+  return {
+    id: result[0].id,
+    name: result[0].name,
+    color: result[0].color,
+    is_default: result[0].is_default,
+    phase: {
+      id: result[0].phase_id,
+      name: result[0].phase_name,
+    },
+  };
 };
 
 export const getWidgetBugsByDuplicates = async (
