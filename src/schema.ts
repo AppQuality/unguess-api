4dd0b2fe486813f0649816919e9eb4239a678c22
Luca Cannarozzo <cannarozzoluca@live.it>
2023-11-19 12:01:27 +0100

    chore(types): update types in schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -539,8 +539,8 @@ export interface components {
     };
     /** Cluster */
     Cluster: {
-      id?: number;
-      name?: string;
+      id: number;
+      name: string;
     };
     /**
      * Coin
