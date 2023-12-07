ea351fec1329a6dd9f6a530a5938176272a6adf7
ZecD <dominik.zecoli@gmail.com>
2023-12-07 18:07:45 +0100

    feat: get bug comments endpoint

---
+++ src/routes/campaigns/campaignId/bugs/bugId/comments/_get/index.ts
@@ -0,0 +1,80 @@
+/** OPENAPI-CLASS: get-campaigns-cid-bugs-bid-comments */
+import { tryber, unguess } from "@src/features/database";
+import BugsRoute from "@src/features/routes/BugRoute";
+import { ERROR_MESSAGE } from "@src/utils/constants";
+import { zonedTimeToUtc } from "date-fns-tz";
+
+type Comment =
+  StoplightOperations["get-campaigns-cid-bugs-bid-comments"]["responses"]["200"]["content"]["application/json"];
+
+export default class Route extends BugsRoute<{
+  response: StoplightOperations["get-campaigns-cid-bugs-bid-comments"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-cid-bugs-bid-comments"]["parameters"]["path"];
+}> {
+  protected async filter(): Promise<boolean> {
+    if (!(await super.filter())) return false;
+
+    if (!this.shouldShowThisBug()) {
+      this.setError(400, {
+        status_code: 400,
+        name: "bugs",
+        message: "You don't have access to this bug",
+      });
+      return false;
+    }
+    return true;
+  }
+
+  protected async prepare() {
+    let comments;
+    try {
+      comments = await this.getBugComments();
+    } catch (e: any) {
+      return this.setError(500, {
+        message: e.message || ERROR_MESSAGE,
+        status_code: 500,
+      } as OpenapiError);
+    }
+    if (!comments.items || !comments.items.length) return this.emptyResponse();
+    return this.setSuccess(200, comments);
+  }
+
+  private async getCreator(creatorId: number) {
+    const result = await tryber.tables.WpAppqEvdProfile.do()
+      .select("name", "surname")
+      .where("wp_user_id", creatorId)
+      .first();
+    if (!result) return null;
+    return `${result.name} ${result.surname}`;
+  }
+
+  private async getBugComments() {
+    const result = await unguess.tables.UgBugsComments.do()
+      .select()
+      .where("bug_id", this.bug_id)
+      .andWhere("is_deleted", 0);
+    const comments = await Promise.all(
+      result.map(async (comment) => ({
+        id: comment.id,
+        text: comment.text,
+        bug_id: comment.bug_id,
+        creation_date: zonedTimeToUtc(
+          comment.creation_date_utc,
+          "UTC"
+        ).toISOString(),
+        creator: {
+          id: comment.creator_id,
+          name: (await this.getCreator(comment.creator_id)) || "",
+        },
+      }))
+    );
+    return {
+      items: comments,
+    };
+  }
+  private emptyResponse() {
+    return this.setSuccess(200, {
+      items: [],
+    });
+  }
+}
