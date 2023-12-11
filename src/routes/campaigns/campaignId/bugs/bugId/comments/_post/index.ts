828d7ee1f0e057e2110834193ce0e126d885c32e
Luca Cannarozzo <cannarozzoluca@live.it>
2023-12-11 10:53:21 +0100

    feat(bug-comment): add basic post comment with tests

---
+++ src/routes/campaigns/campaignId/bugs/bugId/comments/_post/index.ts
@@ -0,0 +1,122 @@
+/** OPENAPI-CLASS: post-campaigns-cid-bugs-bid-comments */
+import BugsRoute from "@src/features/routes/BugRoute";
+import { tryber, unguess } from "@src/features/database";
+import { formatInTimeZone, zonedTimeToUtc } from "date-fns-tz";
+import { formatISO } from "date-fns";
+
+export default class Route extends BugsRoute<{
+  parameters: StoplightOperations["post-campaigns-cid-bugs-bid-comments"]["parameters"]["path"];
+  body: StoplightOperations["post-campaigns-cid-bugs-bid-comments"]["requestBody"]["content"]["application/json"];
+  response: StoplightOperations["post-campaigns-cid-bugs-bid-comments"]["responses"]["200"]["content"]["application/json"];
+}> {
+  private cid: number;
+  private bid: number;
+  private comment: string | undefined;
+
+  constructor(configuration: RouteClassConfiguration) {
+    super(configuration);
+    const params = this.getParameters();
+
+    this.cid = parseInt(params.cid);
+    this.bid = parseInt(params.bid);
+
+    this.comment = this.getBody().text;
+  }
+
+  protected async filter(): Promise<boolean> {
+    if (!(await super.filter())) {
+      this.setError(403, { message: "Something went wrong!" } as OpenapiError);
+      return false;
+    }
+
+    if (!this.comment) {
+      this.setError(400, { message: "Something went wrong!" } as OpenapiError);
+      return false;
+    }
+
+    if (!this.shouldShowThisBug()) {
+      this.setError(400, {
+        status_code: 400,
+        name: "bugs",
+        message: "You don't have access to this bug",
+      });
+      return false;
+    }
+    return true;
+  }
+
+  protected async prepare(): Promise<void> {
+    try {
+      const commentId = await this.addComment();
+      const comment = await this.getComment(commentId);
+
+      if (!comment) {
+        this.setError(400, {
+          message: "Something went wrong!",
+        } as OpenapiError);
+        return;
+      }
+
+      return this.setSuccess(200, {
+        id: comment.id,
+        text: comment.text,
+        creation_date: comment.creation_date_utc,
+        creator: comment.creator,
+      });
+    } catch (error) {
+      switch (error) {
+        case "NOT_FOUND":
+          return this.setError(403, {} as OpenapiError);
+        default:
+          return this.setError(500, {
+            message: error,
+          } as OpenapiError);
+      }
+    }
+  }
+
+  private async getComment(id: number) {
+    const comment = await unguess.tables.UgBugsComments.do()
+      .select("id", "text", "creation_date_utc", "creator_id")
+      .where("id", id)
+      .first();
+
+    if (!comment) return null;
+
+    const author = await tryber.tables.WpAppqEvdProfile.do()
+      .select("id", "name", "surname")
+      .where("id", comment?.creator_id)
+      .first();
+
+    if (!author) return null;
+
+    return {
+      id: comment.id,
+      text: comment.text,
+      creation_date_utc: formatISO(
+        zonedTimeToUtc(comment.creation_date_utc, "UTC")
+      ),
+      creator: {
+        id: author.id,
+        name: `${author.name} ${author.surname}`,
+      },
+    };
+  }
+
+  private async addComment() {
+    const comment = await unguess.tables.UgBugsComments.do()
+      .insert({
+        text: this.comment,
+        bug_id: this.bid,
+        creator_id: this.getProfileId(),
+        creation_date_utc: formatInTimeZone(
+          new Date(),
+          "UTC",
+          "yyyy-MM-dd HH:mm:ss"
+        ),
+      })
+      .returning("id");
+
+    return comment[0].id ?? comment[0];
+  }
+}
