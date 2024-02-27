68022914096546144504c52ae155e5b75c7c3c1b
Marco Bonomo <marco.bonomo@unguess.io>
2024-02-27 12:11:16 +0100

    ✨ feat(mail): add getTemplate function to retrieve email template from database

---
+++ src/features/mail/getTemplate/index.ts
@@ -0,0 +1,38 @@
+import { tryber } from "@src/features/database";
+
+export const getTemplate = async ({
+  template,
+  optionalFields,
+}: {
+  email: string | string[];
+  subject: string;
+  template: string;
+  optionalFields?: { [key: string]: any };
+  categories?: string[];
+}) => {
+  const mailTemplate = await tryber.tables.WpAppqUnlayerMailTemplate.do()
+    .select("html_body")
+    .join(
+      "wp_appq_event_transactional_mail",
+      "wp_appq_event_transactional_mail.template_id",
+      "wp_appq_unlayer_mail_template.id"
+    )
+    .where("wp_appq_event_transactional_mail.event_name", template);
+
+  if (!mailTemplate.length) return "";
+
+  let templateHtml = mailTemplate[0].html_body;
+
+  if (optionalFields) {
+    for (const key in optionalFields) {
+      if (templateHtml.includes(key)) {
+        templateHtml = templateHtml.replace(
+          new RegExp(key, "g"),
+          optionalFields[key as keyof typeof optionalFields]
+        );
+      }
+    }
+  }
+
+  return templateHtml;
+};
